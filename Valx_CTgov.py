8b4a51372a309169ddd7bc75eee0f889fed66720
Chandran Goodchild <ch.goodchild@embedsmart.de>
2023-07-28 14:58:20 +0000

    now reading numeric_features correctly

--- Valx_CTgov.py
+++ Valx_CTgov.py
@@ -47,18 +47,18 @@ def extract_variables (fdin, ffea, ffea2, var):
     Valx_core.init_features()
 
     output = []
-    for i in xrange(len(trials)):
+    for i in range(len(trials)):
         if i%1000 == 0:
             print(('processing %d' % i))
         # pre-processing eligibility criteria text
         text = Valx_core.preprocessing(trials[i][1]) # trials[i][1] is the eligibility criteria text
         (sections_num, candidates_num) = Valx_core.extract_candidates_numeric(text) # extract candidates containing numeric features
-        for j in xrange(len(candidates_num)): # for each candidate
+        for j in range(len(candidates_num)): # for each candidate
             exp_text = Valx_core.formalize_expressions(candidates_num[j]) # identify and formalize values
             (exp_text, key_ngrams) = Valx_core.identify_variable(exp_text, feature_dict_dk, fea_dict_umls) # identify variable mentions and map them to names
             (variables, vars_values) = Valx_core.associate_variable_values(exp_text)
             all_exps = []
-            for k in xrange(len(variables)):
+            for k in range(len(variables)):
                 curr_var = variables[k]
                 curr_exps = vars_values[k]
                 if curr_var in features:
