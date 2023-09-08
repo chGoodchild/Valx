64561bc359b263966269faee9e4489d667278693
Chandran <ch.goodchild@embedsmart.de>
2023-09-08 15:46:21 +0200

    Currently extracting inclusion and exclusion texts based on diabetes.csv

--- sample_usage.py
+++ sample_usage.py
@@ -1,56 +1,20 @@
-import Valx_core
+import argparse
+from Valx_CTgov import extract_variables
+
+def main():
+    parser = argparse.ArgumentParser(description='Extract and structure numeric lab test comparison statements from text using Valx_CTgov')
+    parser.add_argument('-i', default='data/diabetes.csv', help='Input CSV file')
+    parser.add_argument('-f1', default='data/rules.csv', help='Domain knowledge feature list CSV file')
+    parser.add_argument('-f2', default='data/variable_features_umls.csv', help='UMLS feature list CSV file')
+    parser.add_argument('-v', default='All', help='Variable name: All, HBA1C, BMI, Glucose, Creatinine, BP-Systolic, BP-Diastolic')
+
+    args = parser.parse_args()
+
+    extract_variables(args.i, args.f1, args.f2, args.v)
+
+    # result = extract_lab_measurement_info(text)
+    # print("result: ", result)
 
 
-def extract_lab_measurement_info(text):
-
-    # Initialize the features variables from `data/numeric_features.csv`.
-    Valx_core.init_features()
-
-    # The `preprocessing` function cleans and standardizes input text for analysis by
-    # removing special characters, formatting numbers, and reducing whitespace.
-    cleaned_text = Valx_core.preprocessing(text)
-
-
-    # Extract candidates
-    sections_num, candidates_num = Valx_core.extract_candidates_numeric(text)
-
-    print("sections_num: ", sections_num)
-    print("candidates_num: ", candidates_num)
-
-    """
-
-    name_list = "hemoglobin"  # The keyword you want to search for
-    sections, candidates = Valx_core.extract_candidates_name(sections_num, candidates_num, name_list)
-
-    for candidate in candidates:
-        formalized_expression = Valx_core.formalize_expressions(candidate)
-        print(formalized_expression)
-    """
-
-    """
-    # Extract measurement
-    measurement = Valx_core.extract_measurement(cleaned_text)
-
-    # Extract relationship
-    relationship = Valx_core.extract_relationship(cleaned_text)
-
-    # Extract numeric quantity
-    numeric_quantity = Valx_core.extract_numeric_quantity(cleaned_text)
-
-    # Extract units
-    units = Valx_core.extract_units(cleaned_text)
-
-
-    return {
-        "Measurement": measurement,
-        "Relationship": relationship,
-        "Numeric Quantity": numeric_quantity,
-        "Units": units
-    }
-    """
-
-
-# Example usage:
-text = "bla bla, hemoglobin > yy units, bla. More and more text!"
-result = extract_lab_measurement_info(text)
-print("result: ", result)
+if __name__ == '__main__':
+    main()
